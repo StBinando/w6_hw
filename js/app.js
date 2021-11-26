12840d5080d62df3553c799a50e2ad2f9644efb9
Stefano Binando <sbinando@gmail.com>
2021-11-26 15:01:30 +0000

    CSS

--- js/app.js
+++ js/app.js
@@ -8,12 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
     deleteButton.textContent = "wipe board";
     deleteButton.id = "Delete";
     const body = document.querySelector('body')
-    body.appendChild(deleteButton);
+    const accordion = document.querySelector('#accordion');
+    body.insertBefore(deleteButton, accordion);
+    // body.appendChild(deleteButton);
     document.querySelector('#Delete').addEventListener('click', handleDelete);
   }
 
   const handleDelete = function(){
     document.querySelector('ul').innerHTML = "";
+
     const parent = document.querySelector('body');
     const deleteBtn = document.querySelector('button');
     parent.removeChild(deleteBtn);
@@ -25,21 +28,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const newWhisky = document.createElement('li');
     const newName = document.createElement('h3');
     const newRegion = document.createElement('h5');
-    const newAlc = document.createElement('h4');
+    // const newAlc = document.createElement('h4');
     const newPrice = document.createElement('h4');
-
+    
     newWhisky.classList.add('whisky');
+
     newName.textContent = this.name.value;
-    newRegion.textContent = this.region.value;
-    newAlc.textContent = this.alc.value;
-    newPrice.textContent = this.price.value;
+    newName.classList.add('W_name');
+    
+    newRegion.textContent = `(${this.region.value})`;
+    newRegion.classList.add('W_region');
+    
+    // newAlc.textContent = `${this.alc.value}%`;
+    // newAlc.classList.add('W_alc');
+
+    newPrice.textContent = `£${this.price.value}`;
+    newPrice.classList.add('W_price');
+    document.querySelector('#check').checked = false;
 
     const list = document.querySelector('#whisky-list');
     list.appendChild(newWhisky);
     
     newWhisky.appendChild(newName);
     newWhisky.appendChild(newRegion);
-    newWhisky.appendChild(newAlc);
+    // newWhisky.appendChild(newAlc);
     newWhisky.appendChild(newPrice);
     document.querySelector('#new-item-form').reset();
 
